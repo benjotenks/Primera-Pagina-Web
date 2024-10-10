6741266f789081edcea35610d834a7dea2f6dc1d
Benjamin <b.elguetaperez@gmail.com>
2024-10-10 02:18:19 -0300

    Octavo intento con render

--- register.js
+++ register.js
@@ -1,32 +1,41 @@
 document.getElementById('registerForm').addEventListener('submit', async (e) => {
-    e.preventDefault();
+    e.preventDefault(); // Prevenir el comportamiento por defecto del formulario
+
     const email = document.getElementById('RegisterEmail').value;
     const pass = document.getElementById('RegisterPass').value;
-    // 'http://localhost:8090/graphql' 
-    const response = await fetch('https://primera-pagina-web-twl9.onrender.com/graphql', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({
-            query: ` 
-                mutation addUser($input: UserInput) {
-                    addUser(input: $input) {
-                        id
-                        email
-                        pass
-                    }    
-                } 
-            `,
-            variables: {
-                input: {
-                    email: email,
-                    pass: pass,
-                }
-            }
-        }),
-    });
 
-    const result = await response.json();
-    console.log(result);
-})
+    try {
+        const response = await fetch('https://primera-pagina-web-twl9.onrender.com/graphql', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+                query: `
+                    mutation addUser($input: UserInput) {
+                        addUser(input: $input) {
+                            id
+                            email
+                            pass
+                        }    
+                    } 
+                `,
+                variables: {
+                    input: {
+                        email: email,
+                        pass: pass,
+                    }
+                }
+            }),
+        });
+
+        if (!response.ok) {
+            throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+
+        const result = await response.json();
+        console.log('Resultado:', result);
+    } catch (error) {
+        console.error('Error:', error);
+    }
+});
